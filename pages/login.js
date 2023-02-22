efbde44964c18e16d6e1c7749475899ab5ec9625
Deepak R <deepakr@inzerotech.com>
2023-02-22 18:28:01 +0530

    Login script updated

---
+++ pages/login.js
@@ -0,0 +1,20 @@
+exports.loginPage = class loginPage{
+
+    constructor(page){
+        this.page = page
+        this.username_textbox = page.locator("input[name='Email']");
+        this.password_textbox = page.locator("input[name='Password']");
+        this.login_button = page.getByRole("button", { name: "Sign in" });
+    }
+
+    async gotoLoginPage(){
+        await this.page.goto("https://pcxstaging.primetechpa.com/Identity/Login?ReturnUrl=%2F");
+    }
+    
+    async login(username, password){
+        await this.username_textbox.fill(username);
+        await this.password_textbox.fill(password);
+        await this.login_button.click();
+    }
+    
+}
--- tests/LoginTest.spec.js
+++ tests/LoginTest.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { loginPage } from '../pages/login.js';
 
 test.describe("Login Test", () => {
   test.beforeEach(async ({ page }) => {
@@ -12,35 +13,38 @@ test.describe("Login Test", () => {
   });
 
   test("Login Test-Postive Test-User Name/Password", async ({ page }) => {
-    await page.locator("input[name='Email']").fill("deepakr@inzerotech.com");
-    await expect(page).toHaveTitle(/.*PCX - Sign in/);
-    await page.locator("input[name='Password']").fill("Deepak@605");
-    await page.getByRole("button", { name: "Sign in" }).click();
+    const login = new loginPage(page);
+    await login.login('deepakr@inzerotech.com', 'Deepak@605');
     await page.locator(".avatar-initials").click();
     await page.getByText("Sign Out").click();
+
+    // await page.locator("input[name='Email']").fill("deepakr@inzerotech.com");
+    // await expect(page).toHaveTitle(/.*PCX - Sign in/);
+    // await page.locator("input[name='Password']").fill("Deepak@605");
+    // await page.getByRole("button", { name: "Sign in" }).click();
+    // await page.locator(".avatar-initials").click();
+    // await page.getByText("Sign Out").click();
   });
 
   test("Login Test Correct User Name and Incorrect Password-Negative Test", async ({
     page,
   }) => {
-    await page.getByPlaceholder("Email").fill("deepakr@inzerotech.com");
-    await page.getByPlaceholder("Password").fill("xyz");
-    await page.getByRole("button", { name: "Sign in" }).click();    
+    const login = new loginPage(page);
+    await login.login('deepakr@inzerotech.com', 'xyz');
+   
   });
 
   test("Login Test Incorrect User Name and Incorrect Password-Negative Test", async ({
     page,
   }) => {
-    await page.getByPlaceholder("Email").fill("xyz@in.com");
-    await page.getByPlaceholder("Password").fill("xyz");
-    await page.getByRole("button", { name: "Sign in" }).click();
+    const login = new loginPage(page);
+    await login.login('xyz@in.com', 'xyz');
   });
 
   test("Login Test Incorrect User Name and Correct Password-Negative Test", async ({
     page,
   }) => {
-    await page.getByPlaceholder("Email").fill("xyz@in.com");
-    await page.getByPlaceholder("Password").fill("Deepak@605");
-    await page.getByRole("button", { name: "Sign in" }).click();
+    const login = new loginPage(page);
+    await login.login('xyz@in.com', 'Deepak@605');
   });
 });
