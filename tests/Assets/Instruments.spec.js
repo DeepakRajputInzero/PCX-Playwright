4237cee6ad93a489834579a953dd191741c3d84b
Deepak R <deepakr@inzerotech.com>
2023-02-23 19:52:29 +0530

    Updated

--- tests/Assets/Instruments.spec.js
+++ tests/Assets/Instruments.spec.js
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { assets } from "../../pages/assets.js";
+// import { faker } from "@faker-js/faker";
 
 test.describe("Create New Assets record", () => {
     test.beforeEach(async ({ page }) => {
@@ -12,10 +13,11 @@ test.describe("Create New Assets record", () => {
         await page.close();
       });
     test("Create New Instrument record- Manual Test", async ({ page }) => {
+        let date = "2023-02-04"
         const login = new assets(page);
         //Login to Application
         await login.login('deepakr@inzerotech.com', 'Deepak@605');
-        await page.pause();
+        
         //Click on New 
         await page.getByRole('link', { name: ' New' }).click();
         //Click on New Instrument
@@ -28,25 +30,30 @@ test.describe("Create New Assets record", () => {
         await page.locator('#dialog-asset-manufacturer').click();
         await page.locator('#dialog-asset-manufacturer').fill('fluke');
         await page.locator('#ui-id-3').click();
-        await page.locator('select[name=\'PhysicalLocationId\']', 'H&R Plant');
-        const selectValue = await page.$eval("select[name=\'PhysicalLocationId\']", (element) => element.value);
-        expect(selectValue).toContain('H&R Plant');
+        await page.locator('select[name=\'PhysicalLocationId\']').selectOption('51');
+        // const selectValue = await page.$eval("select[name=\'PhysicalLocationId\']", (element) => element.value);
+        // expect(selectValue).toContain('H&R Plant');
         await page.locator('input[name="ModelNumber"]').click();
         await page.locator('input[name="ModelNumber"]').fill('258');
         await page.locator('input[name="FunctionalLocation"]').click();
-        await page.locator('input[name="FunctionalLocation"]').fill('ROOm');
+        await page.locator('input[name="FunctionalLocation"]').fill('Noise Room');
         await page.locator('input[name="SerialNumber"]').click();
         await page.locator('input[name="SerialNumber"]').fill('25886');
-        await page.locator('select[name="DepartmentId"]').selectOption('34');
-        // await page.locator('div').filter({ hasText: 'Classifications' }).locator('div').nth(1).click();
-        // await page.getByText('Classification 1').click();
-        // await page.getByText('General').nth(1).click();
-        // await page.locator('div').filter({ hasText: 'Next Calibration Date' }).locator('i').click();
+        await page.pause();
+        await page.locator('select[name=\"DepartmentId\"]').selectOption('34');
+        await page.locator('.selectize-input').click();
+        await page.getByText('Classification 1').click();
+        await page.getByText('Critical', { exact: true }).click();
+        // await page.locator('#dp1677153209661').click();
         // await page.getByTitle('Next').click();
+        // await page.getByRole('link', { name: '2', exact: true }).click();
+        // await page.locator('select[name="CalibrationFrequencyId"]').selectOption('3');
         // await page.getByRole('row', { name: '1 2 3 4' }).getByRole('cell', { name: '1' }).click();
-        // await page.locator('select[name="CalibrationFrequencyId"]').selectOption('2');
-        await page.locator('#dialog-asset-remarks').click();
-        await page.locator('#dialog-asset-remarks').fill('Calibration');
+        // await page.locator('.date-picker-text hasDatepicker').fill('.date-picker-text hasDatepicker', date);
+        // await page.fill('#dp1677161642359', date)
+        // await page.waitForTimeout(3000)
+        // await page.locator('#dialog-asset-remarks').click();
+        // await page.locator('#dialog-asset-remarks').fill('Calibration');
         // await page.getByRole('listitem').filter({ hasText: 'Test Specifications' }).click();
         // await page.locator('#dialog-test-specification-title').click();
         // await page.locator('#dialog-test-specification-title').fill('Asset Calibration');
@@ -62,9 +69,9 @@ test.describe("Create New Assets record", () => {
         // await page.locator('select[name="TestSpecification\\.ManualPassFail\\.ToleranceTypeId"]').selectOption('2');
         // await page.locator('input[name="TestSpecification\\.ManualPassFail\\.Tolerance"]').click();
         // await page.locator('input[name="TestSpecification\\.ManualPassFail\\.Tolerance"]').fill('5');
-        await page.getByRole('button', { name: 'Save and Close' }).click();
+        // await page.getByRole('button', { name: 'Save and Close' }).click();
 
-        await page.locator(".avatar-initials").click();
-        await page.getByText("Sign Out").click();
+        // await page.locator(".avatar-initials").click();
+        // await page.getByText("Sign Out").click();
     });
 });
